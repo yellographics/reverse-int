92448dcaafcc11745e272f5abebebf07cc0c22a4
Anna Nescierowicz <annie.odesk@gmail.com>
2021-12-29 21:25:44 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+'use strict'
+
+module.exports = function reverse(n) {
+    const reversedString = Array.from(String(Math.abs(n))).reverse().join("")
+    return +reversedString;
 }
+
+
